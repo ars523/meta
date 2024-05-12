162553d50ec00bff5774055e535be3d2d97b956b
Arifur Rahaman <arifur.rahaman.siflu@gmail.com>
2024-05-12 18:20:41 +0600

    updated title

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,12 @@ function App() {
         <title>Og image test</title>
         <meta name="description" content="Brief description of your page content for search engines." />
 
-        <meta property="og:title" content="Your Page Title" />
+        <meta property="og:title" content="My page title" />
         <meta property="og:description" content="Brief description of your page content for social media sharing." />
         <meta property="og:image" content="https://i0.wp.com/old.baahstore.com/wp-content/uploads/2023/12/7_11zon-3.jpg?fit=1200%2C1200&ssl=1" />
         <meta property="og:url" content="https://i0.wp.com/old.baahstore.com/wp-content/uploads/2023/12/7_11zon-3.jpg?fit=1200%2C1200&ssl=1" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="Your Page Title" />
+        <meta name="twitter:title" content="My page title" />
         <meta name="twitter:description" content="Brief description of your page content for Twitter." />
         <meta name="twitter:image" content="https://i0.wp.com/old.baahstore.com/wp-content/uploads/2023/12/7_11zon-3.jpg?fit=1200%2C1200&ssl=1" />
       </Helmet>
