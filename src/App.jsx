96523bb9624c822917202b53797325198b84fae9
Arifur Rahaman <arifur.rahaman.siflu@gmail.com>
2024-05-12 18:27:45 +0600

    updeted

--- src/App.jsx
+++ src/App.jsx
@@ -12,13 +12,14 @@ function App() {
 
         <meta property="og:title" content="My page title" />
         <meta property="og:description" content="Brief description of your page content for social media sharing." />
-        <meta property="og:image" content="https://i0.wp.com/old.baahstore.com/wp-content/uploads/2023/12/7_11zon-3.jpg?fit=1200%2C1200&ssl=1" />
-        <meta property="og:url" content="https://i0.wp.com/old.baahstore.com/wp-content/uploads/2023/12/7_11zon-3.jpg?fit=1200%2C1200&ssl=1" />
+        <meta property="og:image" content="https://i0.wp.com/old.baahstore.com/wp-content/uploads/2023/12/7_11zon-3.jpg" />
+        <meta property="og:url" content="https://yourpageurl.com" /> {/* Replace with your page URL */}
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="My page title" />
         <meta name="twitter:description" content="Brief description of your page content for Twitter." />
-        <meta name="twitter:image" content="https://i0.wp.com/old.baahstore.com/wp-content/uploads/2023/12/7_11zon-3.jpg?fit=1200%2C1200&ssl=1" />
+        <meta name="twitter:image" content="https://i0.wp.com/old.baahstore.com/wp-content/uploads/2023/12/7_11zon-3.jpg" />
       </Helmet>
+
       <div>
         <h1>Hello world</h1>
       </div>
